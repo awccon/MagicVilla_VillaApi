0aa7ade9c8938b894d0bc119f53282f7c2749850
Akmal <awccon@gmail.com>
2024-02-03 20:20:35 -0500

    Add DTO

--- MagicVilla/MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaApi.Models;
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
@@ -9,12 +10,28 @@ namespace MagicVilla_VillaApi.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa> {
-            new Villa{Id =  1, Name = "Pool View"},
-            new Villa{Id = 2, Name = "Beach View"}
-            };
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
         }
     }
 }
--- MagicVilla/MagicVilla_VillaApi/Models/Villa.cs
+++ MagicVilla/MagicVilla_VillaApi/Models/Villa.cs
@@ -1,8 +1,11 @@
-﻿namespace MagicVilla_VillaApi.Models
+﻿using System.Data;
+
+namespace MagicVilla_VillaApi.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
